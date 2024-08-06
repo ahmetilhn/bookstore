52a5724edd2d6c308e4c8c630563fd3db89d0ce1
ahmetilhn <ahmetilhan.dev@gmail.com>
2024-08-06 13:06:34 +0300

    created base nuxt structure

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  // app: {
+  //   head: {
+  //     charset: "utf-16",
+  //     title: "Bookstore - Online bookstore app",
+  //   },
+  // },
+});
