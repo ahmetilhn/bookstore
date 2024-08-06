b3ef1a430e50a3fe3435568d9e4cabf1c087b0da
ahmetilhn <ahmetilhan.dev@gmail.com>
2024-08-06 15:40:07 +0300

    created intro banner

---
+++ components/IntroBanner.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-center mt-10 flex flex-col items-center">
+    <p class="my-4 lg:w-1/2 text-lg text-gray-500">
+      Search for the book you want, add it to your cart and order. The best
+      books are here! Start searching now!
+    </p>
+    <h1
+      class="intro-title w-3/4 lg:text-6xl md:text-5xl sm:text-4xl font-extrabold font-serif text-black"
+    >
+      The most popular books are here!
+    </h1>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.intro-title {
+  line-height: 80px;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <main
     class="w-full flex flex-col justify-center items-center lg:py-10 py-4 px-4 sm:px-6 lg:px-8"
   >
-    <div class="w-full flex flex-col justify-center items-center max-w-7xl">
+    <div class="w-full flex flex-col justify-center items-center max-w-6xl">
       <AppHeader />
       <slot />
     </div>
