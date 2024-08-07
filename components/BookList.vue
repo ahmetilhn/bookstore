6c945241a863e3ea9f32b152742dca0a545d48ca
ahmetilhn <ahmetilhan.dev@gmail.com>
2024-08-07 22:08:17 +0300

    added result not found in book list

--- components/BookList.vue
+++ components/BookList.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="w-full mt-10 flex flex-row flex-wrap">
-    <BookCard
-      v-for="book in books"
-      :id="book.id"
-      :title="book.title"
-      :price="book.price"
-      :author="book.author"
-      :coverImageUrl="book.coverImageUrl"
-      :key="book.id"
-    />
+    <template v-if="books?.length">
+      <BookCard
+        v-for="book in books"
+        :id="book.id"
+        :title="book.title"
+        :price="book.price"
+        :author="book.author"
+        :coverImageUrl="book.coverImageUrl"
+        :key="book.id"
+      />
+    </template>
+    <template v-else>
+      <p class="my-20 w-full text-center text-lg text-gray-400">
+        Result not found
+      </p>
+    </template>
   </div>
 </template>
 <script setup lang="ts">
